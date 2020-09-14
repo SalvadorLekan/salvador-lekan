8d05f2cef4c68aaa0de443d5bcaee57d85d8fcc6
SalvadorLekan <lekansalvador@gmail.com>
2020-09-14 13:47:44 -0400

    First commit

---
+++ tailwind.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    container:{
+      center:true,
+    },
+    extend: {
+      fontFamily:{
+        redHat:['Red Hat Display', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}

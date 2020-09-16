99ab71abf6c6b8d9d76a20ce4f637ae856672cb5
SalvadorLekan <lekansalvador@gmail.com>
2020-09-16 06:19:04 -0400

    Added a landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Home from './pages/Home';
 
 function App() {
+  const [hide, sethide] = useState(true)
+
+
   return (
-    <div className="container max-h-screen relative bg-white text-center">
-      Hello World
+    <div className="container h-screen relative bg-blue-400 flex justify-center">
+     
+     <Home/>
+
+
+     <header className='absolute top-0 w-full z-10'>
+     <nav id="navbar">
+        <div id="logo">logo</div>
+        <div id="lg-menu">
+                <ul>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Experience</a></li>
+                    <li><a href="/">Work</a></li>
+                    <li><a href="/">Contact</a></li>
+                    <li><a href="/">Resume</a></li>
+                </ul>
+            <div id="toggler" className='animate-shake' onClick={()=>{sethide(false)}}>&#9776;</div>
+        </div>
+    </nav>
+    <div id="sm-menu" className={hide?"hide":''}>
+        <div id="hider" onClick={()=>{sethide(true)}}>&times;</div>
+        <ul>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Experience</a></li>
+            <li><a href="/">Work</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href="/">Resume</a></li>
+        </ul>
+    </div>
+     </header>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+    <div className='w-auto slide-in font-bold p-4 sm:p-0 min-h-full inline-flex flex-col justify-center'>
+        <p className='text-3xl'> Hi,</p>
+        <h1 className='text-4xl my-1 p-0 md:text-5xl xl:text-6xl'>My name is Salvador Lekan.</h1>
+        <p className='text-3xl text-right'>A Web Developer.</p>
+     </div>
+    )
+}
+
+export default Home

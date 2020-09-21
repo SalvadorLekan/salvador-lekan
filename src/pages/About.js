3dcbc6ba03036826956b799344ac2f227c0428f7
SalvadorLekan <lekansalvador@gmail.com>
2020-09-21 09:01:23 -0400

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -31,9 +31,9 @@ function About() {
                         Languages.
                     </p>
                     <div className='flex flex-wrap my-3 justify-evenly'>
-                        <section className='flex flex-col items-center'><SiHtml5 className='text-6xl text-red-400'/><p className='text-xl sm:text-2xl sm:font-semibold'>HTML5</p></section>
-                        <section className='flex flex-col items-center'><SiCss3 className='text-6xl text-blue-600'/><p className='text-xl sm:text-2xl sm:font-semibold'>CSS3</p></section>
-                        <section className='flex flex-col items-center'><SiJavascript className='text-6xl text-yellow-400'/><p className='text-xl sm:text-2xl sm:font-semibold'>ES6</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiHtml5 className='text-6xl text-red-400'/><p className='text-xl sm:text-2xl sm:font-semibold'>HTML5</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiCss3 className='text-6xl text-blue-600'/><p className='text-xl sm:text-2xl sm:font-semibold'>CSS3</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiJavascript className='text-6xl text-yellow-400'/><p className='text-xl sm:text-2xl sm:font-semibold'>ES6</p></section>
                     </div>
                 </section>
                 <section>
@@ -41,12 +41,12 @@ function About() {
                         Frameworks.
                     </p>
                     <div className='flex flex-wrap my-3 justify-evenly'>
-                        <section className='flex flex-col items-center'><SiSass className='text-6xl text-red-300'/><p className='text-xl sm:text-2xl sm:font-semibold'>SASS</p></section>
-                        <section className='flex flex-col items-center'><SiReact className='text-6xl text-blue-900'/><p className='text-xl sm:text-2xl sm:font-bold'>React</p></section>
-                        <section className='flex flex-col items-center'><SiGreensock className='text-6xl text-green-600'/><p className='text-xl sm:text-2xl sm:font-bold'>GSAP</p></section>
-                        <section className='flex flex-col items-center'><SiBootstrap className='text-6xl text-purple-800'/><p className='text-xl sm:text-2xl sm:font-bold'>Bootstrap</p></section>
-                        <section className='flex flex-col items-center'><SiRedux className='text-6xl text-purple-700'/><p className='text-xl sm:text-2xl sm:font-bold'>Redux</p></section>
-                        <section className='flex flex-col items-center'><SiTailwindcss className='text-6xl text-green-500'/><p className='text-xl sm:text-2xl sm:font-bold'>TailwindCSS</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiSass className='text-6xl text-red-300'/><p className='text-xl sm:text-2xl sm:font-semibold'>SASS</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiReact className='text-6xl text-blue-900'/><p className='text-xl sm:text-2xl sm:font-bold'>React</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiGreensock className='text-6xl text-green-600'/><p className='text-xl sm:text-2xl sm:font-bold'>GSAP</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiBootstrap className='text-6xl text-purple-800'/><p className='text-xl sm:text-2xl sm:font-bold'>Bootstrap</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiRedux className='text-6xl text-purple-700'/><p className='text-xl sm:text-2xl sm:font-bold'>Redux</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiTailwindcss className='text-6xl text-green-500'/><p className='text-xl sm:text-2xl sm:font-bold'>TailwindCSS</p></section>
                     </div>
                 </section>
                 <section>
@@ -55,9 +55,9 @@ function About() {
                     </p>
                     <p className='my-3 text-xl sm:text-2xl font-serif pt-1'>A result and detail oriented developer who is skilled in making websites accessible to all people. <br/> A team player who is open-minded to new technologies and ideas. <br/> An avid learner with the intention of building solutions to real life problems.</p>
                     <div className='flex flex-wrap my-3 justify-evenly'>
-                        <section className='flex flex-col items-center'><DiResponsive className='text-6xl text-purple-700'/><p className='text-xl sm:text-2xl sm:font-bold'>Responsiveness</p></section>
-                        <section className='flex flex-col items-center'><ImAccessibility className='text-6xl text-purple-700'/><p className='text-xl sm:text-2xl sm:font-bold'>Accessibility</p></section>
-                        <section className='flex flex-col items-center'><SiGithub className='text-6xl text-black'/><p className='text-xl sm:text-2xl sm:font-bold'>Team Projects</p></section>
+                        <section className='my-2 flex flex-col items-center'><DiResponsive className='text-6xl text-purple-700'/><p className='text-xl sm:text-2xl sm:font-bold'>Responsiveness</p></section>
+                        <section className='my-2 flex flex-col items-center'><ImAccessibility className='text-6xl text-purple-700'/><p className='text-xl sm:text-2xl sm:font-bold'>Accessibility</p></section>
+                        <section className='my-2 flex flex-col items-center'><SiGithub className='text-6xl text-black'/><p className='text-xl sm:text-2xl sm:font-bold'>Team Projects</p></section>
                     </div>
                 </section>
             </article> 
